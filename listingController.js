53ba5321836434ee4234044e7059e58dbf7e132e
Luis Carbonell <lt.carbonell@ufl.edu>
2016-01-14 19:18:49 -0500

    Requirements for Assignment 2

--- listingController.js
+++ listingController.js
@@ -7,8 +7,29 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      $scope.listings.push({
+        code: $scope.newCode,
+        name: $scope.newName,
+        coordinates: {
+                latitude: $scope.newCoordinatesLat, 
+                longitude: $scope.newCoordinatesLong
+            },
+        address: $scope.newAddress
+      });
+
+      $scope.newCode = '';
+      $scope.newName = '';
+      $scope.newCoordinatesLat = '';
+      $scope.newCoordinatesLong = '';
+      $scope.newAddress = '';
+    };
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index, 1);
+    };
+    
   }
 ]);
